ca70cc8855a740e65b3de270eadbf8f118f4e750
DKonst21 <k.derienko@gmail.com>
2022-10-12 16:45:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# url = 'https://bitly.is/3RxqFm3'
+
+import requests
+import os
+import argparse
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def shorten_link(token, user_input):
+    headers = {
+        'Authorization': """Bearer {token}""".format(token=token),
+    }
+    long_url = {"long_url": user_input}
+    response = requests.post("""{url}/v4/shorten""".format(
+        url=user_input), headers=headers, json=long_url)
+    response.raise_for_status()
+    return response.json()['link']
+
+
+def count_clicks(token, user_input):
+    headers = {
+        'Authorization': f"Bearer {token}",
+    }
+    params = (
+        ('unit', 'month'),
+        ('units', '-1'),
+    )
+    disassembled_url = urlparse(user_input)
+    net_loc = disassembled_url.netloc
+    path = disassembled_url.path
+    response = requests.get("""https://api-ssl.bitly.com/v4/bitlinks/{net_loc}{path}/clicks/summary""".format(net_loc=net_loc, path=path), headers=headers, params=params)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
+
+    return clicks_count
+
+
+def is_bitlink(token, user_input):
+    headers = {
+        'Authorization': """Bearer {token}""".format(token=token),
+    }
+    disassembled_url = urlparse(user_input)
+    net_loc = disassembled_url.netloc
+    path = disassembled_url.path
+    response = requests.get("""https://api-ssl.bitly.com/v4/bitlinks/{net_loc}{path}""".format(net_loc=net_loc, path=path), headers=headers)
+    return response.ok
+
+
+def main():
+
+    parser = argparse.ArgumentParser('Введите ссылку:')
+    parser.add_argument("link", help='')
+    args = parser.parse_args()
+    # user_input = input('Введите ссылку:')
+    user_input = args.link
+    token = os.environ["BITLY_TOKEN"]
+    # try:
+    if is_bitlink(token, user_input):
+        print('По вашей ссылки прошли:',
+              count_clicks(token, user_input), 'раза', end='\n')
+    else:
+    # except requests.exceptions.HTTPError:
+        print('Битлинк', shorten_link(token, user_input))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Bitly URL shortener
+Getting the number of clicks on a short link.
+
+### How to install
+
+Python3 should already be installed. Use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+pip install -r requirements.txt
+
+it is recommended to use [virtualenv/venv](https://docs.python.org/3/library/venv.html) for project isolation
+
+### ject Goals
+This code was written for educational purposes as part of an online course for web developers at 
+[dvmn.org](https://dvmn.org/).
